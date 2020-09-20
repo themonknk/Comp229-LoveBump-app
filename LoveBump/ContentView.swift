a35598f9da43b0b8729d05c507904d913bdbf827
Nakul <test>
2020-09-20 15:45:46 +0300

    Initial Commit

---
+++ LoveBump/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoveBump
+//
+//  Created by Nakul Khanna on 20/9/20.
+//  Copyright © 2020 Midnightheat nk.inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
